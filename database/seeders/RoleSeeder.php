2eedadcaf23d95488a0204dbcc2d6917caf9e3d9
Juan Felipe Toro <felipe.toro710@gmail.com>
2023-09-02 14:43:45 -0500

    Seeders finish

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = new Role;
+        $role -> name = 'Admin';
+        $role -> description = 'Administrador del sistema';
+        $role -> save();
+
+        $role = new Role;
+        $role -> name = 'Guest';
+        $role -> description = 'Invitado del sistema';
+        $role -> save();
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User;
+        $user -> fullname = 'Juan';
+        $user -> email = 'Juan@gmail.com';
+        $user -> phone = '3103707415';
+        $user -> photo = 'images/no-photo.png';
+        $user -> password = '123';
+        $user -> role_id = 1;
+        $user -> save();
+
+        $user = new User;
+        $user -> fullname = 'jesus';
+        $user -> email = 'nazaret@gmail.com';
+        $user -> phone = '31037075';
+        $user -> photo = 'img/no-photo.png';
+        $user -> password = '321';
+        $user -> role_id = 2;
+        $user -> save();
+
+        for ($i=0; $i < 100; $i++){
+            $user = new User;
+            $user -> fullname = "User $i";
+            $user -> email = "$i@gmail.com";
+            $user -> phone = "3103707415$i";
+            $user -> photo = 'images/no-photo.png';
+            $user -> password = "123$i";
+            $user -> role_id = 2;
+            $user -> save();
+        }
+    }
+}
